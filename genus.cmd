adc92f1a9930d094ff2fcf0ca6a828ce6cd83e2c
Tung <thanhtunglk1@gmail.com>
2025-05-18 21:47:28 +0700

    update

--- genus.cmd
+++ genus.cmd
@@ -1,6 +1,6 @@
 # Cadence Genus(TM) Synthesis Solution, Version GENUS15.20 - 15.20-p004_1, built Nov 14 2015
 
-# Date: Sat May 17 05:33:08 2025
+# Date: Sun May 18 10:43:28 2025
 # Host: centos7 (x86_64 w/Linux 3.10.0-1160.108.1.el7.x86_64) (3*12th Gen Intel(R) Core(TM) i5-12500H 18432KB)
 # OS:   CentOS Linux release 7.9.2009 (Core)
 
